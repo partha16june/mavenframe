6f1b835b04c15072bd5b2b78df96ec0cd4bcff5b
partha16june <parthamechsarathi@gmail.com>
2021-11-30 20:34:46 +0530

    messagehai

--- src/test/java/org/login/Amazon.java
+++ src/test/java/org/login/Amazon.java
@@ -51,6 +51,10 @@ public static void main(String[] args) throws FileNotFoundException {
 	String x = cell.getStringCellValue();
 	
 	System.out.println(x);
+	System.out.println(x);
+	System.out.println(x);
+	System.out.println(x);
+	
 	
 	
 
